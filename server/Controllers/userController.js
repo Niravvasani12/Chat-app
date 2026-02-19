a830d6e0bda67dc14e923d86298b8539da0cd7dd
Niravvasani12 <Niravvasani12@users.noreply.github.com>
2026-02-19 12:29:38 +0530

    Register, login, find , get User API

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -8,23 +8,23 @@ const createToken = (_id) => {
   return jwt.sign({ _id }, jwtkey, { expiresIn: "3d" });
 };
 
+//*Register user api
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password)
-      return res.status(400).json("All fields are required");
+      return res.status(400).json({ message: "All fields are required" });
 
     let user = await userModel.findOne({ email });
 
-    if (user)
-      return res.status(400).json("User with the given email already exists");
+    if (user) return res.status(400).json({ message: "User already exists" });
 
     if (!validator.isEmail(email))
-      return res.status(400).json("Email must be valid");
+      return res.status(400).json({ message: "Email must be valid" });
 
     if (!validator.isStrongPassword(password))
-      return res.status(400).json("Password must be strong");
+      return res.status(400).json({ message: "Password must be strong" });
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
@@ -37,16 +37,70 @@ const registerUser = async (req, res) => {
 
     const token = createToken(user._id);
 
-    res.status(200).json({
+    res.status(201).json({
       _id: user._id,
-      name,
-      email,
+      name: user.name,
+      email: user.email,
       token,
     });
   } catch (error) {
-    console.log(error);
-    res.status(500).json("Something went wrong");
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
   }
 };
 
-module.exports = { registerUser };
+//*Login user api
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({ message: "All fields are required" });
+
+    const user = await userModel.findOne({ email });
+
+    if (!user)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword)
+      return res.status(400).json({ message: "Invalid email or password" });
+
+    const token = createToken(user._id);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Login failed" });
+  }
+};
+
+//*Find user api
+const findUser = async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const user = await userModel.findById(userId);
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+};
+
+//*get user api
+const getUsers = async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+};
+module.exports = { registerUser, loginUser, findUser, getUsers };
