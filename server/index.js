cadf9cc29f961fc2fde47dba7713a1c3ff495851
Niravvasani12 <Niravvasani12@users.noreply.github.com>
2026-02-23 12:58:01 +0530

    Chat API create

--- server/index.js
+++ server/index.js
@@ -4,12 +4,17 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const userRoute = require("./Routes/userRoute");
+const chatRoute = require("./Routes/chatRoute");
+const messageRoute = require("./Routes/messageRoute");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+
 app.get("/", (req, res) => {
   res.send("Welcome to our chat app APIs.");
 });
